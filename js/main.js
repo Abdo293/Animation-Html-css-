b45f56b894d6f330cf4353aa43f59658b9b31f58
Abdo293 <abdoelsayed293@gmail.com>
2024-02-11 02:15:28 +0200

    added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let dox = document.querySelector("dsd");
+let man = document.querySelector("dsd");
+let, (m = document.querySelector("dsd"));
+let, (m = document.querySelector("dsd"));
+let, (m = document.querySelector("dsd"));
+let, (m = document.querySelector("dsd"));
+console.log("skd");
+let, (m = document.querySelector("dsd"));
